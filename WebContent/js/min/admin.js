a52898dc1138c380fb8acab8d4f88f8981ba6628
Mike Meng(HOME) <ytu_mxh@sina.cn>
2015-07-24 07:16:21 +0800

    js Signed-off-by: Mike Meng(HOME) <ytu_mxh@sina.cn>

--- WebContent/js/min/admin.js
+++ WebContent/js/min/admin.js
@@ -52,7 +52,7 @@ $(function(){
 		                   {"sTitle": "警衔津贴","mData":"jxjtAllowance"},{"sTitle": "教护龄津贴","mData":"jhljtAllowance"},{"sTitle": "班主任津贴","mData":"bzrAllowance"},{"sTitle": "改革性补贴二-物业补贴","mData":"ggxbtWybtAllowance"},
 		                   {"sTitle": "其他补助工资","mData":"qtbzAllowance"},{"sTitle": "考评奖","mData":"kpAward"},{"sTitle": "其他工资","mData":"qtSalary"},{"sTitle": "独生子女费","mData":"dsznAllowance"},
 		                   {"sTitle": "补发工资应发额","mData":"bfgzyfSalary"},{"sTitle": "住房补贴","mData":"zfAllowance"},{"sTitle": "扣发小计","mData":"kfTotal"},{"sTitle": "扣公积金","mData":"kgjjMoney"},
-		                   {"sTitle": "扣养老保险","mData":"kyalbxMoney"},{"sTitle": "扣医疗保险","mData":"kyilbxMoney"},{"sTitle": "大病救助金","mData":"dbbzjMoney"},{"sTitle": "应纳所得税额","mData":"iitMoney"},
+		                   {"sTitle": "扣养老保险","mData":"kyalbxMoney"},{"sTitle": "扣医疗保险","mData":"kyilbxMoney"},{"sTitle": "职业年金","mData":"dbbzjMoney"},{"sTitle": "应纳所得税额","mData":"iitMoney"},
 		                   {"sTitle": "扣所得税","mData":"kiitMoney"},{"sTitle": "月份","mData":"monthSalary"},{"sTitle": "年度","mData":"yearSalary"},{"sTitle": "是否计税","mData":"sfjsTax"}
 						  ];
 		$("#tab01").dataTable({
